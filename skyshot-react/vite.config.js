d749656286c3bca97385c560095b80b9705b47d0
Fede Secchi <federico.secchimarino@gmail.com>
2025-12-06 01:16:30 +0100

    chore: clean up config files and update GitHub Pages workflow

--- skyshot-react/vite.config.js
+++ skyshot-react/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// Trigger redeploy
 export default defineConfig({
   base: '/skyshot/',
-  plugins: [react()],
-});
+  plugins: [react()]
+})
