fc65978fc1676d983c27e3e7924710891423b0a8
Fede Secchi <federico.secchimarino@gmail.com>
2025-12-06 01:11:25 +0100

    chore: trigger GitHub Pages redeploy after activation

--- skyshot-react/vite.config.js
+++ skyshot-react/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// Trigger redeploy
 export default defineConfig({
   base: '/skyshot/',
   plugins: [react()],
